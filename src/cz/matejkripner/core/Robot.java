deb7aa86264a02fc38a187dc60f516e6ffa62947
Matěj Kripner <kripnermatej@gmail.com>
2015-04-14 20:05:57 +0200

    Added methods
    Added some methods signatures to the Robot class.

--- src/cz/matejkripner/core/Robot.java
+++ src/cz/matejkripner/core/Robot.java
@@ -1,8 +1,28 @@
 package cz.matejkripner.core;
 
+import java.util.function.Predicate;
+
 /**
  * @author Matìj Kripner <kripnermatej@gmail.com>
  * @version 1.0
  */
 public class Robot {
+
+    private int expectedDirection;
+
+    public boolean perform(Movement movement) {
+
+    }
+
+    public void test(Measurement measurement) {
+
+    }
+
+    public void waitFor() {
+
+    }
+
+    public void doWhile(Movement movement, Predicate<Robot> whileSatisfied) {
+
+    }
 }
